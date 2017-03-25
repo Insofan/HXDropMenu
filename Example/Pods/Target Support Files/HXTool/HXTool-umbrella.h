c724e3e2d8b23e50c534c459444acf5ed1bd7f22
Insofan <282132315@qq.com>
2017-03-26 00:45:09 +0800

    [add]完成第一版

---
+++ Example/Pods/Target Support Files/HXTool/HXTool-umbrella.h
@@ -0,0 +1,19 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "HXButtonTool.h"
+#import "HXColorTool.h"
+#import "HXTool.h"
+
+FOUNDATION_EXPORT double HXToolVersionNumber;
+FOUNDATION_EXPORT const unsigned char HXToolVersionString[];
+
